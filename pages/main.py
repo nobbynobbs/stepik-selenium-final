71488182f04fc7c6c301e8e44219f97e3a3c1278
nobbynobbs <rbolkhovitin@gmail.com>
2019-09-08 19:17:27 +0300

    add locators

--- pages/main.py
+++ pages/main.py
@@ -1,15 +1,16 @@
 from selenium.webdriver.common.by import By
 
 from pages.base import BasePage
+from pages.locators import MainPageLocators
 
 
 class MainPage(BasePage):
     
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
 
     def should_be_login_link(self):
-        want_true = self._is_element_present(By.CSS_SELECTOR, "#login_link")
+        want_true = self._is_element_present(*MainPageLocators.LOGIN_LINK)
         error_msg = "Login link is not presented" 
         assert want_true, error_msg
